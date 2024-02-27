a09055e2c407ebaa5963d670a9ac267838587a83
Takuto Maekawa <148175105+Malin502@users.noreply.github.com>
2024-02-27 14:11:05 +0900

    完成版
    データセットの画像が回転・反転していた問題を解決

--- Learn_EMNIST.py
+++ Learn_EMNIST.py
@@ -5,29 +5,38 @@ from torchvision import datasets, transforms
 import torch.onnx
 import torch.nn.functional as F
 
-epochs = 15
+epochs = 12
 batch_size = 64
+
+
+transform = transforms.Compose([
+    transforms.RandomAffine([90, 110]),
+    transforms.RandomHorizontalFlip(p = 1),
+    transforms.ToTensor()
+])
+
+
 
 #EMINIStを読み込み
 train_data = datasets.EMNIST(
     './EMINIST',
     split='letters',
     train=True, download=True,
-    transform=transforms.ToTensor())
+    transform=transform)
 
 test_data = datasets.EMNIST(
     './EMINIST',
     split='letters',
     train=False, download=True,
-    transform=transforms.ToTensor())
+    transform=transform)
 
 traindata_loader = torch.utils.data.DataLoader(train_data, batch_size = batch_size,
                                                shuffle=True, pin_memory=True)
 testdata_loader = torch.utils.data.DataLoader(test_data, batch_size = batch_size,
-                                              shuffle=True, pin_memory=True)
+                                              shuffle=False, pin_memory=True)
 
-#device = "cuda:0" if torch.cuda.is_available() else "cpu"
-device = "cuda:0"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+#device = "cpu"
 
 print("Using device: " + device)
 
@@ -37,26 +46,33 @@ class NN(nn.Module):
     
     def __init__(self):
         super(NN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1) 
-        self.conv2 = nn.Conv2d(32, 128, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(128*7*7, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 26)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5,padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(), 
+            nn.MaxPool2d(2))
+        
+        self.fc1 = nn.Linear(32*7*7, 27)
 
     def forward(self, x):
         x = x.to(device)
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = x.view(-1, 128*7*7)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        
+        #print(x.shape)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
         return x
     
 model = NN().to(device)
 
 loss_fn =  nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.005)
+optimizer = optim.SGD(model.parameters(), lr=0.01)
 
 
 def train(dataloader, model, loss_fn, optimizer):
@@ -66,6 +82,8 @@ def train(dataloader, model, loss_fn, optimizer):
         X, y = X.to(device), y.to(device)
         
         pred = model(X)
+        #print(pred.shape, y.shape)
+        
         loss = loss_fn(pred, y)
         
         optimizer.zero_grad()
@@ -75,6 +93,8 @@ def train(dataloader, model, loss_fn, optimizer):
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]") 
+            
+            
             
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
@@ -92,6 +112,9 @@ def test(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
+    
+    
     
 #学習と検証
 for t in range(epochs):
